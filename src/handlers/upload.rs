f4dd0b0a812296830b5e7cb327dbcf88ef46e76d
FlareFlo <mail@flareflo.dev>
2025-04-22 11:37:39 +0200

    Add WIP expiration

--- src/handlers/upload.rs
+++ src/handlers/upload.rs
@@ -3,6 +3,7 @@ use crate::error_compat::error_compat::InternalServerErrorExt;
 use crate::file_meta::FileMeta;
 use crate::retention_control::delete_asset;
 use axum::extract::Multipart;
+use axum::extract::multipart::Field;
 use axum::response::{IntoResponse, Redirect};
 use tokio::fs;
 use tokio::fs::File;
@@ -25,24 +26,38 @@ pub async fn accept_form(multipart: Multipart) -> ApiResult<impl IntoResponse> {
 
 async fn do_upload(mut multipart: Multipart, out_dir: &str) -> ApiResult<impl IntoResponse> {
     while let Some(mut field) = multipart.next_field().await.unwrap() {
-        // let name = field.name().unwrap().to_string();
-        let file_name = field.file_name().unwrap().to_string();
-        let content_type = field.content_type().unwrap().to_string();
-
-        let mut metadata = FileMeta::default_policy(file_name.clone(), content_type.clone());
-        let mut f = File::create(format!("{out_dir}/{file_name}"))
-            .await
-            .unwrap();
-        while let Some(chunk) = field.chunk().await.unwrap() {
-            metadata.add_size(chunk.len() as u64);
-            f.write_all(&chunk).await.ise()?;
+        dbg!(&field);
+        let name = field.name().unwrap().to_string();
+        match name.as_str() {
+            "file" => {payload_field(field, out_dir).await?}
+            "expiration" => {
+                todo!()
+            }
+            _ => {
+                panic!("Unknown field {name}")
+            }
         }
-        fs::write(
-            format!("{out_dir}/meta.toml"),
-            toml::to_string_pretty(&metadata).unwrap().as_bytes(),
-        )
+    }
+    Ok(())
+}
+
+async fn payload_field(mut field: Field<'_>, out_dir: &str) -> ApiResult<()> {
+    let file_name = field.file_name().unwrap().to_string();
+    let content_type = field.content_type().unwrap().to_string();
+
+    let mut metadata = FileMeta::default_policy(file_name.clone(), content_type.clone());
+    let mut f = File::create(format!("{out_dir}/{file_name}"))
         .await
         .unwrap();
+    while let Some(chunk) = field.chunk().await.unwrap() {
+        metadata.add_size(chunk.len() as u64);
+        f.write_all(&chunk).await.ise()?;
     }
+    fs::write(
+        format!("{out_dir}/meta.toml"),
+        toml::to_string_pretty(&metadata).unwrap().as_bytes(),
+    )
+    .await
+    .unwrap();
     Ok(())
 }

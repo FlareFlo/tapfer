585b72f95cd1fcbf7d6be516092aecaaef1588f0
FlareFlo <mail@flareflo.dev>
2025-05-12 14:35:34 +0200

    Also add qrsize to homepage

--- src/handlers/homepage.rs
+++ src/handlers/homepage.rs
@@ -1,12 +1,14 @@
 use askama::Template;
 use axum::response::{Html, IntoResponse};
+use crate::configuration::QR_CODE_SIZE;
 
 #[derive(Template)]
 #[template(path = "homepage.html")]
 pub struct Homepage {
     embed_image_url: &'static str,
+    qr_size: usize,
 }
 
 pub async fn show_form() -> impl IntoResponse {
-    Html(Homepage{ embed_image_url: "/graphics/favicon.ico" }.render().unwrap())
+    Html(Homepage{ embed_image_url: "/graphics/favicon.ico", qr_size: QR_CODE_SIZE }.render().unwrap())
 }
